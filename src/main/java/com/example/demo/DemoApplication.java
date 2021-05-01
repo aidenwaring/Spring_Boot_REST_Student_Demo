b927c0fa506d32c6f1e0884926133fbb57d54b0b
awaring <aiden.waring@corelogic.com.au>
2021-05-01 16:35:51 +1000

    Extracted StudentController information out of main file and into Controller, providing a new rest controller specifically for the student, specified the @RequestMapping path and a @GetMapping GET route.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,32 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Year;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-			new Student(
-			1L,
-			"Mario",
-			"mario@nintendo.com",
-			LocalDate.of(1985, Month.SEPTEMBER, 5), 35
-			)
-		);
-	}
 }
