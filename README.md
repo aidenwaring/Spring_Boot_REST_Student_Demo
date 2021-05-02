03a1851f65074759dd357b29674171f9aaa0e131
awaring <aiden.waring@corelogic.com.au>
2021-05-02 14:16:55 +1000

    Provided Delete and Put routes and services, commenting and updated readme.

--- README.md
+++ README.md
@@ -4,17 +4,46 @@ This is a Spring Boot sandbox application used for testing and educational purpo
 
 This is a work in progress.
 
+This service demonstrates basic CRUD operations in a
+simple Spring Boot App built using Spring Initializer.
+
 ### Prerequisites
 
 - JDK 11+
 - Maven
 - PostgreSQL Database
 
-### Installing
+## Installing
 
-## Student Database
+### Student Database
 
 Start a local instance of PostgreSQL.
 
 ```
 docker run --rm -it -p 5432:5432 --name postgres -e POSTGRES_PASSWORD="test123" -e POSTGRES_USER="awaring" -e POSTGRES_DB=student -d postgres
+```
+
+## Scratches
+
+```
+GET http://localhost:8080/api/v1/student
+```
+
+```
+POST http://localhost:8080/api/v1/student
+Content-Type: application/json
+
+{
+  "name": "Link",
+  "email": "link@nintendo.com",
+  "dob": "1986-02-21"
+}
+```
+
+```
+DELETE http://localhost:8080/api/v1/student/delete/1
+```
+
+```
+PUT http://localhost:8080/api/v1/student/update/1?name=Luigi&email=luigi@nintendo.com
+```
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,14 +3,16 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
     /*
-    @Service is a @Component Spring annotation, though Service is instead used here
-    for semantics to more easily identify that this Spring Component is a Service class.
+        @Service is a @Component Spring annotation, though Service is instead used here
+        for semantics to more easily identify that this Spring Component is a Service class.
     */
 
 public class StudentService {
@@ -33,5 +35,57 @@ public class StudentService {
             throw new IllegalStateException("email taken");
         }
         studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException(
+                "student with id " + studentId + "does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    /*
+        Transactional annotation is used here as the set values using the Entity's get and set values
+        enter a managed state and are provided to the database using JPA in this fashion.
+
+        The transactional annotation itself defines the scope of a single database transaction. The database transaction happens inside the scope of apersistence context.
+
+        The persistence context is in JPA the EntityManager, implemented internally using an
+        Hibernate Session (when using Hibernate as the persistence provider).
+
+        The persistence context is just a synchronizer object that tracks the state of a
+        limited set of Java objects and makes sure that changes on those objects are eventually
+        persisted back into the database.
+
+        From: https://dzone.com/articles/how-does-spring-transactional
+
+        CRUD methods on the repository are transactional by default.
+     */
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+            .orElseThrow(() -> new IllegalStateException(
+                "student with id " + studentId + "does not exist")
+            );
+        /*
+            If the param provided name variable is not null, has a length greather than 0 and
+            the Object's name (checked by using the student entity's getter method aren't the same,
+            use the entity's setter method to set the value to the param variable provided
+         */
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+
+            Optional<Student> studentOptional = studentRepository
+                .findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email already exists");
+            }
+            student.setEmail(email);
+        }
     }
 }
